99d320ef4b3f83cc906878f46ca6b1d7998679ec
Artem Sitd <tym3@yandex.ru>
2024-03-03 13:40:48 +0500

    fix

--- contracts/tasks.py
+++ contracts/tasks.py
@@ -1,19 +1,6 @@
-import sys
 from .models import Contract
 from django.utils import timezone
 from celery import shared_task
-
-
-# @shared_task
-# def test5sec():
-#     sys.stdout.write('task 5 sec')
-#     print('task 5 sec')
-#
-#
-# @shared_task
-# def test10sec():
-#     sys.stdout.write('task 10 sec')
-#     print('task 10 sec')
 
 @shared_task
 def check_validity_contracts():
--- crm_django/celery.py
+++ crm_django/celery.py
@@ -1,22 +1,13 @@
 import os
-
 from celery import Celery
-
-# Set the default Django settings module for the 'celery' program.
 from celery.schedules import crontab
-from datetime import timedelta
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm_django.settings')
 
 app = Celery('crm_django')
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
 # можно тут настройки оставить, либо общие в settings.py
@@ -25,12 +16,4 @@ app.conf.beat_schedule = {
         'task': 'contracts.tasks.check_validity_contracts',
         'schedule': crontab(hour='0', minute='0'),  # Задача будет выполняться ежедневно в полночь
     },
-    # 'task5sec': {
-    #     'task': 'contracts.tasks.test5sec',
-    #     'schedule': timedelta(seconds=5),
-    # },
-    # 'task10sec': {
-    #     'task': 'contracts.tasks.test10sec',
-    #     'schedule': timedelta(seconds=10)
-    # }
 }
